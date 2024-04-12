7f4eb1a40d0d034abcec56fd2ccd4c3b9e137323
bemoe10 <mohammed.bendahmane@somosinapsis.com>
2024-04-12 21:59:52 +0200

    first commit

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const NavBar = () => {
+
+
+    return (
+        <div className='bg-gray-200 py-4'>
+            Navigation Bar
+        </div>
+    )
+}
+
+export default NavBar
