4de3900da94faba76a5cf52a185e8e68c0c5f76b
bemoe10 <mohammed.bendahmane@somosinapsis.com>
2024-04-13 06:54:10 +0200

    sidebar footer

---
+++ app/components/layout/SideBarFooter.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+const SideBarFooter = () => {
+  return (
+      <div className='flex flex-row justify-center gap-6 mt-auto pt-8 pb-4 '>
+          <a href="https://www.github.com/" target='_blank'>
+              <Image src="/assets/icons/github.png" alt='piracy' width={24} height={24} quality={100}></Image>
+          </a>
+          <a href="https://www.patreon.com/" target='_blank'>
+              <Image src="/assets/icons/patreon.png" alt='piracy' width={24} height={24} quality={100}></Image>
+          </a>
+          <a href="https://www.reddit.com/" target='_blank'>
+              <Image src="/assets/icons/reddit.png" alt='piracy' width={24} height={24} quality={100}></Image>
+          </a>
+    </div>
+  )
+}
+
+export default SideBarFooter
