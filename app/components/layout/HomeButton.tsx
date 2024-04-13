5e658a3cdcc38528f9bea4e9d7471242e65b2312
bemoe10 <mohammed.bendahmane@somosinapsis.com>
2024-04-13 06:53:31 +0200

    extracted home button component from navbar

---
+++ app/components/layout/HomeButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeButton = () => {
+    return (
+        <div className="flex justify-center py-8">
+            <a className="link-hover text-xl" href='/'>PiracyWiki</a>
+        </div>
+    )
+}
+
+export default HomeButton
