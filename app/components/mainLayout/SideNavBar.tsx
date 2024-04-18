175ca7eba346d632051f343b99c5c8cdfe3082e3
bemo <b.hamdidou97@gmail.com>
2024-04-18 17:41:47 +0200

    side navbar dynamically generated from object

--- app/components/mainLayout/SideNavBar.tsx
+++ app/components/mainLayout/SideNavBar.tsx
@@ -1,80 +1,114 @@
 import React from 'react'
+import Link from 'next/link'
+
+interface NavigationItem {
+    name : string,
+    path : string,
+    sections?: Array<NavigationItem>
+}
 
 const SideBar = () => {
+    const streaming : NavigationItem = {
+        name: 'Streaming',
+        path: '#streaming'
+    };
+    const directDownload : NavigationItem = {
+        name: 'Direct Downloads',
+        path: '#direct-downloads'
+    };
+    const torrent : NavigationItem = {
+        name: 'Torrents',
+        path: '#torrents'
+    };
+
+    const menu : Array<NavigationItem> = [
+        {
+            name: 'General Purpose',
+            path: "/general-purpose",
+            sections: []
+        },
+        {
+            name: 'Software',
+            path: "/software",
+            sections: [
+                directDownload,
+                torrent
+            ]
+        },
+        {
+            name: 'Video Games',
+            path: "/video-games",
+            sections: [
+                directDownload,
+                torrent
+            ]
+        },
+        {
+            name: 'Movies & TV Shows',
+            path: "/movies-and-tv-shows",
+            sections: [
+                streaming,
+                directDownload,
+                torrent
+            ]
+        },
+        {
+            name: 'Anime',
+            path: "/anime",
+            sections: [
+                streaming,
+                directDownload,
+                torrent
+            ]
+        },
+        {
+            name: 'Music',
+            path: "/music",
+            sections: [
+                directDownload,
+                torrent
+            ]
+        },
+        {
+            name: 'Books',
+            path: "/books",
+            sections: [
+                directDownload,
+                torrent
+            ]
+        },
+        {
+            name: 'Sports',
+            path: "/sports",
+            sections: [
+                streaming,
+                directDownload,
+                torrent
+            ]
+        }
+    ];
+
     return (
-        <>
-            <div className="ml-2">
-                <ul className="menu text-base font-medium text-slate-600">
-                    <li><a>General Purpose</a></li>
+        <div className="ml-4">
+            <ul className="menu text-base font-medium text-slate-600">
+                {menu.map((category) =>
                     <li>
                         <details>
-                            <summary><a>Software</a></summary>
+                            <summary>
+                                <Link className='hover:link' href={'/category/'+category.path}>{category.name}</Link>
+                            </summary>
                             <ul>
-                                <li><a>Direct Downloads</a></li>
-                                <li><a>Torrents</a></li>
+                                {category.sections?.map(section =>
+                                    <li>
+                                        <Link scroll={true} className='hover:link' href={'/category'+category.path+section.path}>{section.name}</Link>
+                                    </li>
+                                )}
                             </ul>
                         </details>
                     </li>
-                    <li>
-                        <details>
-                            <summary><a>Video Games</a></summary>
-                            <ul>
-                                <li><a>Direct Downloads</a></li>
-                                <li><a>Torrents</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <details>
-                            <summary><a>Movies & TV Shows</a></summary>
-                            <ul>
-                                <li><a>Streaming</a></li>
-                                <li><a>Direct Downloads</a></li>
-                                <li><a>Torrents</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <details>
-                            <summary><a>Anime</a></summary>
-                            <ul>
-                                <li><a>Streaming</a></li>
-                                <li><a>Direct Downloads</a></li>
-                                <li><a>Torrents</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <details>
-                            <summary><a>Music</a></summary>
-                            <ul>
-                                <li><a>Direct Downloads</a></li>
-                                <li><a>Torrents</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <details>
-                            <summary><a>Books</a></summary>
-                            <ul>
-                                <li><a>Direct Downloads</a></li>
-                                <li><a>Torrents</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <details>
-                            <summary><a>Sports</a></summary>
-                            <ul>
-                                <li><a>Streaming</a></li>
-                                <li><a>Direct Downloads</a></li>
-                                <li><a>Torrents</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
-            </div>
-        </>
+                )}
+            </ul>
+        </div>
     )
 }
 
