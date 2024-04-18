00ae7252d2a3c2de3255249817f6a3921537820d
bemo <b.hamdidou97@gmail.com>
2024-04-18 17:41:24 +0200

    a tag to next link

--- app/components/mainLayout/HomeButton.tsx
+++ app/components/mainLayout/HomeButton.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 import Image from 'next/image'
 
 const HomeButton = () => {
     return (
         <div className="flex justify-center items-center gap-2 py-8">
             <Image src="/assets/images/piracy.png" alt='piracy' width={50} height={50} quality={100} />
-            <a className="link-hover text-xl" href='/'>PiracyWiki</a>
+            <Link className="link-hover text-xl" href='/'>PiracyWiki</Link>
         </div>
     )
 }
