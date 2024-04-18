700013336469f8a1fd092358a37cf29bf838f372
bemo <b.hamdidou97@gmail.com>
2024-04-18 17:42:19 +0200

    test

--- app/components/mainLayout/HomeButton.tsx
+++ app/components/mainLayout/HomeButton.tsx
@@ -7,7 +7,7 @@ const HomeButton = () => {
         <div className="flex justify-center items-center gap-2 py-8">
             <Image src="/assets/images/piracy.png" alt='piracy' width={50} height={50} quality={100} />
             <Link className="link-hover text-xl" href='/'>PiracyWiki</Link>
-        </div>
+        </div> 
     )
 }
 
